f05285ae0b10b4187ef11e7b4a7f1de3d208940e
ArrobaRicardoGE <ricardoarroba@outlook.com>
2022-05-30 22:37:37 -0700

    [Inputs] Agregar soporte para hacer click en las piezas. Ahora lo que se vaya a seleccionar necesita un collider. Fue agregado a cada prefab como prueba

---
+++ CHESSpecial/Assets/Scripts/Clickable.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clickable : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit, 100.0f))
+            {
+                if (hit.transform)
+                {
+                    ClickAction(hit.transform.gameObject);
+                }
+            }
+        }
+    }
+   
+    void ClickAction(GameObject obj)
+    {
+        print(obj); 
+    }
+}
