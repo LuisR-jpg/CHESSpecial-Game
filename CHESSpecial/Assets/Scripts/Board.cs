15e89025536748b27bcc71c5f6ff93180b94b54c
LalitoR-jpg <laloro08@hotmail.com>
2022-06-05 11:40:36 -0700

    [Mechanics] Implement Dynamic Board
    -Rough implementation of a dynamically sized board.
    
    -Each cell is an individual game object with its Collider for click support

---
+++ CHESSpecial/Assets/Scripts/Board.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Board: MonoBehaviour
+{
+
+    public GameObject whiteCell;
+    public GameObject blackCell;
+    public int nRows = 8;
+    public int nCols = 8;
+    void Start()
+    {
+        //Instantiate(whiteCell, new Vector3(0, 0, 0), Quaternion.identity);
+
+        // GameObject a = Instantiate(blackCell);
+        // Vector3 newRotation = new Vector3(-90, 0, 0);
+        // a.transform.position = new Vector3(0.5f, 0, 0.5f);
+        // a.transform.eulerAngles = newRotation;
+        GameObject cell;
+        for(int i = 0; i < nRows; i++){
+            for(int j = 0; j < nCols; j++){
+                cell = Instantiate(((i + j) % 2 == 1)? blackCell: whiteCell);
+                cell.name = (char)(i + 'A') + (j + 1).ToString();
+                cell.transform.position = new Vector3(i - 3.5f, 0, j - 3.5f);
+                cell.transform.eulerAngles = new Vector3(-90, 0, 0);
+            }
+        }
+    }
+
+}
