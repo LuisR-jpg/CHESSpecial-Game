75018a6d5db6d626fb812367d255521b5e06aaf1
LalitoR-jpg <laloro08@hotmail.com>
2022-06-05 21:43:38 -0700

    [Mechanics] Movement prototype
    -Resize pieces to fit in the new board. New prefabs were created to avoid overwriting the previous ones
    
    -[Test] Added rigidbody and implemented movement for a bishop at cell A1

--- CHESSpecial/Assets/Scripts/Board.cs
+++ CHESSpecial/Assets/Scripts/Board.cs
@@ -7,25 +7,36 @@ public class Board: MonoBehaviour
 
     public GameObject whiteCell;
     public GameObject blackCell;
+    public GameObject piece;
     public int nRows = 8;
     public int nCols = 8;
     void Start()
     {
-        //Instantiate(whiteCell, new Vector3(0, 0, 0), Quaternion.identity);
-
-        // GameObject a = Instantiate(blackCell);
-        // Vector3 newRotation = new Vector3(-90, 0, 0);
-        // a.transform.position = new Vector3(0.5f, 0, 0.5f);
-        // a.transform.eulerAngles = newRotation;
         GameObject cell;
         for(int i = 0; i < nRows; i++){
             for(int j = 0; j < nCols; j++){
                 cell = Instantiate(((i + j) % 2 == 1)? blackCell: whiteCell);
                 cell.name = (char)(i + 'A') + (j + 1).ToString();
-                cell.transform.position = new Vector3(i - 3.5f, 0, j - 3.5f);
+                cell.transform.position = new Vector3(i - (float)nRows/2 - 0.5f, 0, j - (float)nCols/2 - 0.5f);
                 cell.transform.eulerAngles = new Vector3(-90, 0, 0);
+                if(cell.name == "A1"){ 
+                    GameObject p = Instantiate(piece);
+                    p.transform.position = cell.transform.position;
+                    StartCoroutine(Move(p));
+                }
             }
         }
     }
-
+    IEnumerator Move(GameObject p)
+    {
+        while(true) {
+            float prevX = p.transform.position.x;
+            p.GetComponent<Rigidbody>().velocity = new Vector3(5, 0, 0);
+            print("hola");
+            yield return new WaitUntil(() => p.transform.position.x >= prevX + 1f);
+            p.GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 0);
+            print("adios");
+            yield return new WaitForSeconds(1);
+        }
+    }
 }
