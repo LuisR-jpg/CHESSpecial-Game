3193abd8be21ec515f7a08ba7440591f2c49bf4e
LalitoR-jpg <laloro08@hotmail.com>
2022-06-14 21:37:44 -0700

    14 de Junio

--- CHESSpecial/Assets/Scripts/QueenController.cs
+++ CHESSpecial/Assets/Scripts/QueenController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -51,6 +52,7 @@ public class QueenController : PieceBaseController
         {
             aState = 0;
             if (currentlyAttacking == null) return;
+            Debug.Log(power);
             currentlyAttacking.GetComponent<PieceBaseController>().Damage(power);
         }
         else if (aState == 0)
